f92e1768a5f565c13c293e94d8cadcd7248675cd
Daedra <foglzerika@gmail.com>
2021-02-11 06:10:48 -0600

    feat | Resolve change

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             sizes: ['S', 'M', 'L'],
-            variants:[
-                {id: 2233 , color: 'green'},
-                {id: 2234, color: 'blue'}
-            ]
+            variants: [
+                {id: 2233, color: 'green', image: './assets/images/socks_blue.jpg'},
+                {id: 2234, color: 'blue', image: './assets/images/socks_green.jpg'}
+            ],
+
         }
+    },
+    methods: {
+        addCart() {
+            this.cart += 1;
+        },
+        removeCar() {
+            this.cart -= 1;
+        },
+        updateImage(variantImage) {
+            console.log(variantImage)
+            this.image=(variantImage)
+        },
     }
 })
     
